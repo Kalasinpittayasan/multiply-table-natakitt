ffbc0acf6710cc5f857dfa963df40fd1e8da78f7
natakitt <47376@kalasinpit.ac.th>
2025-02-10 15:59:22 +0700

    Add files via upload

--- multiplyTable.c
+++ multiplyTable.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main() {
     int a;
-    scanf("%d",&a);
-    for(int i = 1; i <= 12; i++) {
-        printf("%d x %d\n",  a,i,(a*i));
-        }
+    scanf("%d", &a);
+    for (int i = 1; i <= 12; i++) {
+        printf("%d x %d = %d\n", a, i, a * i);
     }
+    return 0;
+}
